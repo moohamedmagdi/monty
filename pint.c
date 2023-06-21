1cc49eed7100482a918492137009b1e1119b5c88
moohamedmagdi <moohamedmagdi@gmail.com>
2023-06-20 22:21:53 -0700

    add free_stack

--- pint.c
+++ pint.c
@@ -12,6 +12,7 @@ void f_pint(stack_t **head, unsigned int counter)
 		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
 		fclose(bus.file);
 		free(bus.content);
+		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*head)->n);
