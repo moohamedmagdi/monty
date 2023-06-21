5e937d1b97ab2ec6a5e15c91f86ea7c70c1ceb0c
moohamedmagdi <moohamedmagdi@gmail.com>
2023-06-20 21:42:03 -0700

    delete puch

--- pop.c
+++ pop.c
@@ -14,7 +14,6 @@ void f_pop(stack_t **head, unsigned int counter)
 		fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
 		fclose(bus.file);
 		free(bus.content);
-		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
 	h = *head;
--- swap.c
+++ swap.c
@@ -21,7 +21,6 @@ void f_swap(stack_t **head, unsigned int counter)
 		fprintf(stderr, "L%d: can't swap, stack too short\n", counter);
 		fclose(bus.file);
 		free(bus.content);
-		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
 	h = *head;
