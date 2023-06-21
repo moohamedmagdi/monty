e1db4addc0a516e4646c64aca8a846ecc028ab88
moohamedmagdi <moohamedmagdi@gmail.com>
2023-06-21 08:19:17 -0700

    error getline

--- main.c
+++ main.c
@@ -10,7 +10,9 @@ int main(int argc, char *argv[])
 {
 	char *content;
 	FILE *file;
+	size_t size = 0;
 	ssize_t read_line = 1;
+	stack_t *stack = NULL;
 	unsigned int counter = 0;
 
 	if (argc != 2)
@@ -28,10 +30,16 @@ int main(int argc, char *argv[])
 	while (read_line > 0)
 	{
 		content = NULL;
+		read_line = getline(&content, &size, file);
 		bus.content = content;
 		counter++;
+		if (read_line > 0)
+		{
+			execute(content, &stack, counter, file);
+		}
 		free(content);
 	}
+	free_stack(stack);
 	fclose(file);
 return (0);
 }
